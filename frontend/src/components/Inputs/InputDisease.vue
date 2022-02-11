04e14cd77427fce192cd5de255d3e5727fb4669a
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-11 16:46:10 +0100

    Add InputDisease

---
+++ frontend/src/components/Inputs/InputDisease.vue
@@ -0,0 +1,34 @@
+<template>
+<v-container>
+    <v-text-field
+    label="Disease (breast cancer)"  
+    dense
+    outlined
+    v-model="disease"
+    v-on:input="handleInput()"
+    type="text"
+    />
+</v-container>
+</template>
+
+
+<script>
+export default {
+    props:{
+        value: String,
+    },
+    data: function(){
+        return{
+            disease: this.value,
+        }
+    },
+    computed:{
+        //CODE
+    },
+    methods:{
+      handleInput: function(){
+        this.$emit('input',this.disease);
+      },
+    },
+}
+</script>
