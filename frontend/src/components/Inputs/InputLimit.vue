8a18bfe6e2458e4767ebd864fe612a144775250b
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-15 15:16:30 +0100

    Add InputLimit

---
+++ frontend/src/components/Inputs/InputLimit.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-checkbox
+    dense
+    class="ma-0 pa-0"
+    v-model="keepLimitID"
+    :label="label"
+    @click="handleInput()"
+    />
+</template>
+
+
+<script>
+
+export default {
+    props:{
+        value: Boolean,
+        limitType: String,
+    },
+    data: function(){
+        let d  = {
+            keepLimitID: this.value,
+        };
+        return d;
+    },
+    computed:{
+        label: function(){
+            return `Keep ${this.limitType} Limit ID`;
+        }
+    },
+    methods:{
+      handleInput: function(){
+        this.$emit('input',this.keepLimitID);
+      },
+    },
+    mounted: function(){
+        //CODE
+    }
+}
+</script>
