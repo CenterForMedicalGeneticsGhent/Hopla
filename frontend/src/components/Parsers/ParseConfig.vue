5028969dc5bed756c626dfcee746d06bc56056f7
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-16 17:08:54 +0100

    Add ParseSectionSampleDiseaseAnnotation

--- frontend/src/components/Parsers/ParseConfig.vue
+++ frontend/src/components/Parsers/ParseConfig.vue
@@ -4,6 +4,7 @@
     <ParseSectionImportant :config="config" />
     <ParseSectionVariantInclusionFilter1 :config="config" />
     <ParseSectionVariantInclusionFilter2 :config="config" />
+    <ParseSectionSampleDiseaseAnnotation :config="config" />
 </div>
 </template>
 
@@ -13,8 +14,9 @@ import Vue from "vue";
 
 import ParseSectionMandatory from "../Parsers/ParseSectionMandatory.vue";
 import ParseSectionImportant from "../Parsers/ParseSectionImportant.vue";
-import ParseSectionVariantInclusionFilter1 from "../Parsers/ParseSectionVariantInclusionFilter1.vue"
-import ParseSectionVariantInclusionFilter2 from "../Parsers/ParseSectionVariantInclusionFilter2.vue"
+import ParseSectionVariantInclusionFilter1 from "../Parsers/ParseSectionVariantInclusionFilter1.vue";
+import ParseSectionVariantInclusionFilter2 from "../Parsers/ParseSectionVariantInclusionFilter2.vue";
+import ParseSectionSampleDiseaseAnnotation from "../Parsers/ParseSectionSampleDiseaseAnnotation.vue";
 
 export default Vue.extend({    
     props:{
@@ -25,6 +27,7 @@ export default Vue.extend({
         ParseSectionImportant,
         ParseSectionVariantInclusionFilter1,
         ParseSectionVariantInclusionFilter2,
+        ParseSectionSampleDiseaseAnnotation,
     },
     data: function(){
         return {
