f95c10b43aace86dcb1e91234e8fa6e47dd2f8be
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-18 13:49:52 +0100

    Add ParseAfHardLimit

---
+++ UI/src/components/Parsers/ParseAfHardLimit.vue
@@ -0,0 +1,29 @@
+<template>
+<span>
+    af.hard.limit={{content}}<br>
+</span>
+</template>
+
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({    
+    props:{
+        afHardLimit: Number,
+    },
+    components: {
+    },
+    data: function(){
+        return {
+        }
+    },
+    computed:{
+        content: function(){
+            return `${this.afHardLimit}`;
+        }
+    },
+    methods:{
+    },
+})
+</script>
--- UI/src/components/Parsers/ParseSectionVariantInclusionFilter1.vue
+++ UI/src/components/Parsers/ParseSectionVariantInclusionFilter1.vue
@@ -3,6 +3,7 @@
     <ParseSectionVariantInclusionFilter1Title />
     <ParseDpHardLimitIDs :configPedigree="config.configPedigree" />
     <ParseAfHardLimitIDs :configPedigree="config.configPedigree" />
+    <ParseAfHardLimit :afHardLimit="config.configParameters.afHardLimit" />
 </span>
 </template>
 
@@ -13,6 +14,7 @@ import Vue from "vue";
 import ParseSectionVariantInclusionFilter1Title from "../Parsers/ParseSectionVariantInclusionFilter1Title.vue"
 import ParseDpHardLimitIDs from "../Parsers/ParseDpHardLimitIDs.vue";
 import ParseAfHardLimitIDs from "../Parsers/ParseAfHardLimitIDs.vue";
+import ParseAfHardLimit from "./ParseAfHardLimit.vue"
 
 export default Vue.extend({    
     props:{
@@ -22,6 +24,7 @@ export default Vue.extend({
         ParseSectionVariantInclusionFilter1Title,
         ParseDpHardLimitIDs,
         ParseAfHardLimitIDs,
+        ParseAfHardLimit,
     },
     data: function(){
         return {
