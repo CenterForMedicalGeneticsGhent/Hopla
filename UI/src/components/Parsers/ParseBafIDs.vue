0a05e19de52a448b835673072f692e492fae53ff
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-18 18:04:43 +0100

    Add ParseBafIDs

---
+++ UI/src/components/Parsers/ParseBafIDs.vue
@@ -0,0 +1,64 @@
+<template>
+<span>
+    baf.ids={{content}}<br>
+</span>
+</template>
+
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({    
+    props:{
+        configPedigree: Object,
+    },
+    components: {
+    },
+    data: function(){
+        return {
+        }
+    },
+    computed:{
+        paternalGrandfather: function(){
+            return "";
+        },
+        paternalGrandmother: function(){
+            return "";
+        },
+        maternalGrandfather: function(){
+            return "";
+        },
+        maternalGrandmother: function(){
+            return "";
+        },
+        father: function(){
+            return "";
+        },
+        mother: function(){
+            return "";
+        },
+        siblings: function(){
+            return "";
+        },
+        embryos: function(){
+            var embryoIDs = this.configPedigree.configEmbryos.embryoList.map(function(d){return d.sampleID});
+            var embryoString="";
+            for (let i=0; i<embryoIDs.length; i++){
+                embryoString+=`${embryoIDs[i]},`;
+            }
+            return embryoString;
+        },
+        content: function(){
+            var content=`${this.paternalGrandfather}${this.paternalGrandmother}${this.maternalGrandfather}${this.maternalGrandmother}${this.father}${this.mother}${this.siblings}${this.embryos}`;
+            if (content){
+                return content.slice(0,-1);
+            }
+            else {
+                return "";
+            }
+        }
+    },
+    methods:{
+    },
+})
+</script>
