d9ca7648acb231e6506fa87fd80837420aaa1251
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-24 14:16:15 +0100

    Validate chrEnd input

--- UI/src/components/Inputs/InputChrEnd.vue
+++ UI/src/components/Inputs/InputChrEnd.vue
@@ -17,15 +17,28 @@ export default {
     data: function(){
         return{
             chrEnd: this.value,
+            chrEndOld: this.value,
         }
     },
     computed:{
         //CODE
     },
     methods:{
-      handleInput: function(){
-        this.$emit('input',Math.ceil(Number(this.chrEnd)));
-      },
+        handleInput: function(){
+                var chrEndNew = Math.ceil(Number(this.chrEnd)); //If not number it is transformed to 0
+                if (chrEndNew===this.chrEndOld){
+                    //Nothing changes, do nothing
+                }
+                else if (chrEndNew>0){
+                    // Valid input
+                    this.chrEndOld=chrEndNew;
+                    this.$emit('input',chrEndNew);
+                } 
+                else {
+                    //Invalid input, change back to previous value
+                    this.chrEnd = this.chrEndOld;
+                } 
+        },
     },
 }
 </script>
