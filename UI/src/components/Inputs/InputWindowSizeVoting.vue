25a1a09c8444a3fa4c928d6aa597fe9e50d053cd
Jan Van den Schilden <jan.vandenschilden@ugent.be>
2022-02-24 14:31:36 +0100

    Validate windowSizeVoting

--- UI/src/components/Inputs/InputWindowSizeVoting.vue
+++ UI/src/components/Inputs/InputWindowSizeVoting.vue
@@ -17,15 +17,28 @@ export default {
     data: function(){
         return{
             windowSizeVoting: this.value,
+            windowSizeVotingOld: this.value,
         }
     },
     computed:{
         //CODE
     },
     methods:{
-      handleInput: function(){
-        this.$emit('input',Math.floor(Number(this.windowSizeVoting)));
-      },
+        handleInput: function(){
+            var windowSizeVotingNew = Number(this.windowSizeVoting); //If not number it is transformed to 0
+            if (windowSizeVotingNew===this.windowSizeVotingOld){
+                //Nothing changes, do nothing
+            }
+            else if (windowSizeVotingNew>0){
+                // Valid input
+                this.windowSizeVotingOld=windowSizeVotingNew;
+                this.$emit('input',windowSizeVotingNew);
+            } 
+            else {
+                //Invalid input, change back to previous value
+                this.windowSizeVoting = this.windowSizeVotingOld;
+            } 
+        },
     },
 }
 </script>
