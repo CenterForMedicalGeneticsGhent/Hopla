9ccbb5a9341699f002b40b6fae90b75f479dbdd5
janvandenschilden <41264522+janvandenschilden@users.noreply.github.com>
2022-05-06 16:09:15 +0200

    Bugfix2 reload of settings file adds additional sibling (#7)
    * Fix Siblings
    
    * Fix Embryos
    
    Co-authored-by: Jan Van den Schilden <jan.vandenschilden@ugent.be>

--- UI/src/components/Parsers/Config2Form/extractInfoEmbryos.js
+++ UI/src/components/Parsers/Config2Form/extractInfoEmbryos.js
@@ -10,42 +10,44 @@ export default function extractInfoEmbryos(paramsObject, config){
     
     // Retrieve Params
     var sampleIDs=paramsObject.pedigreeMapping.embryos;
-    var indicesOfID=sampleIDs.map(function(d){
-        return determinePositionSampleID(d,paramsObject["sample.ids"]);
-    }); 
-    var genders=indicesOfID.map(function(i){
-        return paramsObject["genders"][i];
-    });
-    var keepLimitIDHardDPs=sampleIDs.map(function(d){
-        return "hide";
-    }); 
-    var keepLimitIDHardAFs=sampleIDs.map(function(d){
-        return "hide";
-    });
-    var keepLimitIDSoftDPs=sampleIDs.map(function(d){
-        return determinekeepLimitIDSoftDP(d,paramsObject["dp.soft.limit.ids"]);
-    }); 
-    var keepInformativeIDs=sampleIDs.map(function(d){
-        return "hide";
-    });
-    var keepHeteroIDs=determineKeepHeteroIDs(paramsObject["keep.hetero.ids"]);
-    var diseaseStati=sampleIDs.map(function(d){
-        return determineDiseaseStatus(d,paramsObject["carrier.ids"],paramsObject["affected.ids"],paramsObject["nonaffected.ids"]);
-    });
+    if (!sampleIDs[0]==""){ //check whether there are embryos
+        var indicesOfID=sampleIDs.map(function(d){
+            return determinePositionSampleID(d,paramsObject["sample.ids"]);
+        }); 
+        var genders=indicesOfID.map(function(i){
+            return paramsObject["genders"][i];
+        });
+        var keepLimitIDHardDPs=sampleIDs.map(function(d){
+            return "hide";
+        }); 
+        var keepLimitIDHardAFs=sampleIDs.map(function(d){
+            return "hide";
+        });
+        var keepLimitIDSoftDPs=sampleIDs.map(function(d){
+            return determinekeepLimitIDSoftDP(d,paramsObject["dp.soft.limit.ids"]);
+        }); 
+        var keepInformativeIDs=sampleIDs.map(function(d){
+            return "hide";
+        });
+        var keepHeteroIDs=determineKeepHeteroIDs(paramsObject["keep.hetero.ids"]);
+        var diseaseStati=sampleIDs.map(function(d){
+            return determineDiseaseStatus(d,paramsObject["carrier.ids"],paramsObject["affected.ids"],paramsObject["nonaffected.ids"]);
+        });
 
-    // Assign Params
-    for (let i=0; i<sampleIDs.length;i++){
-        config.configPedigree.configEmbryos.embryoList.push({})
-        config.configPedigree.configEmbryos.embryoList[i].sampleID=sampleIDs[i];
-        config.configPedigree.configEmbryos.embryoList[i].gender=genders[i];
-        config.configPedigree.configEmbryos.embryoList[i].keepLimitIDHardDP=keepLimitIDHardDPs[i];
-        config.configPedigree.configEmbryos.embryoList[i].keepLimitIDHardAF=keepLimitIDHardAFs[i];
-        config.configPedigree.configEmbryos.embryoList[i].keepLimitIDSoftDP=keepLimitIDSoftDPs[i];
-        config.configPedigree.configEmbryos.embryoList[i].keepInformativeIDs=keepInformativeIDs[i];
-        config.configPedigree.configEmbryos.embryoList[i].diseaseStatus=diseaseStati[i];
+        // Assign Params
+        for (let i=0; i<sampleIDs.length;i++){
+            config.configPedigree.configEmbryos.embryoList.push({})
+            config.configPedigree.configEmbryos.embryoList[i].sampleID=sampleIDs[i];
+            config.configPedigree.configEmbryos.embryoList[i].gender=genders[i];
+            config.configPedigree.configEmbryos.embryoList[i].keepLimitIDHardDP=keepLimitIDHardDPs[i];
+            config.configPedigree.configEmbryos.embryoList[i].keepLimitIDHardAF=keepLimitIDHardAFs[i];
+            config.configPedigree.configEmbryos.embryoList[i].keepLimitIDSoftDP=keepLimitIDSoftDPs[i];
+            config.configPedigree.configEmbryos.embryoList[i].keepInformativeIDs=keepInformativeIDs[i];
+            config.configPedigree.configEmbryos.embryoList[i].diseaseStatus=diseaseStati[i];
+        }
     }
+    
     config.configPedigree.configEmbryos.keepHeteroIDs=keepHeteroIDs;
-
     return config;
     
 }
