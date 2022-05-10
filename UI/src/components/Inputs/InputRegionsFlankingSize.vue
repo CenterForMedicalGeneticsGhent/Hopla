8155b1355902c610728ad99a052fa0b1505f0829
janvandenschilden <41264522+janvandenschilden@users.noreply.github.com>
2022-05-10 14:39:56 +0200

    Enhancement.004 add field regionsflankingsize (#15)
    * Add InputRegionsFlankingSize UI Component
    
    * Changed parser Config2Form to read regions.flanking.size
    
    Co-authored-by: Jan Van den Schilden <jan.vandenschilden@ugent.be>

---
+++ UI/src/components/Inputs/InputRegionsFlankingSize.vue
@@ -0,0 +1,38 @@
+<template>
+<v-autocomplete
+v-model="regionsFlankingSize"
+:items="options"
+label="Regions Flanking Size"
+outlined
+/>
+</template>
+
+
+<script>
+export default {
+    props:{
+        value: Number,
+    },
+    data: function(){
+        return {
+            options: [
+                1000000,
+                2000000,
+            ],
+        }
+    },
+    computed:{
+        regionsFlankingSize: {
+            get:function(){
+                return this.value;
+            },
+            set: function(d){
+                this.$emit('input',d);
+            },
+        },
+    },
+    methods:{
+        //CODE
+    },
+}
+</script>
