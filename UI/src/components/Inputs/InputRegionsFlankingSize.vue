b23a46ad8c0b563fa9830b0fc92ab0754e4eeff3
Jan Van den Schilden <jan.vandenschilden@gmail.com>
2023-05-17 14:40:27 +0200

    Add regions.flanking.size input

---
+++ UI/src/components/Inputs/InputRegionsFlankingSize.vue
@@ -0,0 +1,42 @@
+<template>
+<v-text-field
+label="Regions Flankinh Size"
+outlined
+v-model="regionsFlankingSize"
+type="number"
+:key="key"
+/>
+</template>
+
+
+<script>
+export default {
+    props:{
+        value: Number,
+    },
+    data: function(){
+        return {
+            key:0,
+        }
+    },
+    computed:{
+        regionsFlankingSize:{
+            get: function(){
+                return this.value;
+            },
+            set: function(d){
+                if (isNaN(d) || Number(d)<=0 ){
+                    this.$emit('input',this.regionsFlankingSize);
+                    this.key++ //Update view and reset to previous valid value
+                }
+                else {
+                    this.$emit('input',Number(d));
+                }
+            }
+        },
+    },
+    methods:{
+        //CODE
+    },
+}
+</script>
